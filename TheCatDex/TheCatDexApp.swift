fdcba4eea2e3fe9f67f8daea1f9d8675d2db9e14
Matheus Feola <matheus.feola@ebankit.com>
2025-03-01 16:54:01 +0000

    Initial commit. Create base Xcode project

---
+++ TheCatDex/TheCatDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheCatDexApp.swift
+//  TheCatDex
+//
+//  Created by Matheus Feola on 01/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct TheCatDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
